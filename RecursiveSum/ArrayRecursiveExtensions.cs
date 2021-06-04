d32ec6a96cba60430df4bb4a22a02cb272726811
rafael.monteiro <rafael.monteiro@segfy.com>
2021-06-04 08:43:11 -0300

    max and sun Algorithms

--- RecursiveSum/ArrayRecursiveExtensions.cs
+++ RecursiveSum/ArrayRecursiveExtensions.cs
@@ -11,5 +11,19 @@
 
             return ((dynamic)values[0]) + Sum(values[1..]);
         }
+
+        public static T Max<T>(this T[] values) where T : struct
+        {
+            dynamic subMax = values[0];
+
+            if (values.Length == 2)
+            {
+                return (dynamic)values[0] > values[1] ? values[0] : values[1];
+            }
+
+            subMax = Max(values[1..]);
+
+            return (dynamic)values[0] > subMax ? values[0] : subMax;
+        }
     }
 }
--- RecursiveSum/Program.cs
+++ RecursiveSum/Program.cs
@@ -6,9 +6,10 @@ namespace RecursiveSum
     {
         public static void Main(string[] args)
         {
-            float[] vals = { 1, 2, 3, 4, 5, 6, 7 };
+            float[] vals = { 1, 2, 3, 4, 5, 6, 7, 99 };
 
             Console.WriteLine(vals.Sum());
+            Console.WriteLine(vals.Max());
         }
 
         
